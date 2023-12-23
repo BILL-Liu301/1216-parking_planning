d732b4b9207ea48e5bf7f0ce6bfac97547de8b04
“BILL-Liu301” <1409688109@qq.com>
2023-12-23 21:28:42 +0800

    02：修改了初始化文件的程序，使其更加智能

--- pythons/init_folders.py
+++ pythons/init_folders.py
@@ -1,20 +1,31 @@
 import os
+import shutil
 
 from modules.base_path import path_base, path_dataset
 from modules.base_path import path_pts, path_solutions
 from modules.base_path import path_figs, path_figs_init, path_figs_train, path_figs_test, path_figs_once, path_figs_failed, path_figs_all
 
-os.mkdir(path_base)
+print(f"警告！警告！即将删除“{path_base}”！！！")
+print(f"请进行确认！！！")
+flag = input("请进行确认（True/确认删除，False/不删除）：")
 
-os.mkdir(path_dataset)
-os.mkdir(path_pts)
-os.mkdir(path_solutions)
+if flag:
+    print("正在删除...")
+    shutil.rmtree(path_base)
 
-os.mkdir(path_figs)
-os.mkdir(path_figs_all)
-os.mkdir(path_figs_init)
-os.mkdir(path_figs_train)
-os.mkdir(path_figs_test)
-os.mkdir(path_figs_once)
-os.mkdir(path_figs_failed)
+    os.mkdir(path_base)
+
+    os.mkdir(path_dataset)
+    os.mkdir(path_pts)
+    os.mkdir(path_solutions)
+
+    os.mkdir(path_figs)
+    os.mkdir(path_figs_all)
+    os.mkdir(path_figs_init)
+    os.mkdir(path_figs_train)
+    os.mkdir(path_figs_test)
+    os.mkdir(path_figs_once)
+    os.mkdir(path_figs_failed)
+else:
+    print("已停止删除程序...")
 
